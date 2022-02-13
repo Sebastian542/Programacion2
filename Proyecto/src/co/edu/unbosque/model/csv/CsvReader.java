74bf085de7297d92bd7bc11963055acd6a1a2e9f
Juan Sebastian Muñoz <SebastianM@Laptop>
2022-02-12 20:10:46 -0500

    Lectura de Datos csv funcionando.

--- Proyecto/src/co/edu/unbosque/model/csv/CsvReader.java
+++ Proyecto/src/co/edu/unbosque/model/csv/CsvReader.java
@@ -1,25 +1,38 @@
 package co.edu.unbosque.model.csv;
 
+import com.opencsv.CSVReader;//external library
+import java.io.FileReader; //Reads text from character files using a default buffer size
 
-import com.opencsv.bean.CsvToBean;
-import com.opencsv.bean.CsvToBeanBuilder;
 
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.util.List;
-
-public class CsvReader {
-
+public class CsvReader{
 	
-	    public List<Producto> readCsvFile() throws FileNotFoundException {
-	        FileReader reader =  new FileReader("Data/data.csv");
-	        
-	        CsvToBean<Producto> csvtobean= new CsvToBeanBuilder<Producto>(reader)
-	                .withType(Producto.class)
-	                .withSeparator(',')
-	                .build();
+private static String LOCATION_OF_THE_FILE="C:\\Users\\SebastianM\\git\\Programacion2\\Proyecto\\Data\\data.csv";//file location
 
-	        return csvtobean.parse();
-	    }
-	}
+	public static void main(String[] args)
+	{
+    LineByLine(LOCATION_OF_THE_FILE);
+        }
+    public static void LineByLine(String myfile)
+      {
+       try
+           {
+           FileReader freader= new FileReader(myfile);//created an object of freader class
+        @SuppressWarnings("resource")
+        CSVReader creader= new CSVReader(freader);// created creader object by parsing freader as a parameter
+           String [] nextRecord;// created an array of type String
+         //read data line by line
+        while((nextRecord = creader.readNext())!=null)
+           {
+               for(String token: nextRecord)
+               System.out.println(token +"\t"); //will bring the value of cell seperated by tab space
+           }
+           System.out.println();
+           }
+          catch(Exception e) //to catch any exception inside try block
+       {
+          e.printStackTrace();//used to print a throwable class along with other dataset class
+  }
 
+}
+
+}
--- Proyecto/src/co/edu/unbosque/model/csv/CsvWriter.java
+++ Proyecto/src/co/edu/unbosque/model/csv/CsvWriter.java
@@ -1,38 +1,22 @@
 package co.edu.unbosque.model.csv;
 
-
-import com.opencsv.CSVWriter;
-import com.opencsv.bean.CsvToBeanBuilder;
-import com.opencsv.bean.StatefulBeanToCsv;
-import com.opencsv.bean.StatefulBeanToCsvBuilder;
-import com.opencsv.exceptions.CsvDataTypeMismatchException;
-import com.opencsv.exceptions.CsvRequiredFieldEmptyException;
-
-import java.io.FileWriter;
-import java.io.IOException;
-import java.io.Writer;
+import java.util.ArrayList;
 import java.util.List;
 
-
 public class CsvWriter {
+	
 
-    public void writeSpaceships() throws IOException, CsvDataTypeMismatchException, CsvRequiredFieldEmptyException, InterruptedException {
-        Writer writer  = new FileWriter("Data/data.csv");
+	    private List<Producto> csvList;
 
 
-        StatefulBeanToCsv<Producto> beanToCsv = new StatefulBeanToCsvBuilder<Producto>(writer)
-                .withSeparator(',')
-                .withLineEnd(CSVWriter.DEFAULT_LINE_END)
-                .withOrderedResults(true)
-                .build();
 
-//        List<SpaceShip> spaceShips = List.of(new SpaceShip(10, "Mike", 1, "20190724")
-//                , new SpaceShip(11, "Big Bottle", 134, "20170524")
-//                , new SpaceShip(12, "Castle", 2435, "20180721"));
-//
-//        beanToCsv.write(spaceShips);
-//        beanToCsv.write(new SpaceShip(13, "Test", 2435, "20180721"));
-//        beanToCsv.getCapturedExceptions();
-//        writer.close();
-    }
+	    public void setCsvList(List<Producto> csvList) {
+	        this.csvList = csvList;
+	    }
+
+	    public List<Producto> getCsvList() {
+	        if (csvList != null) return csvList;
+	        return new ArrayList<>();
+	    }
+
 }
